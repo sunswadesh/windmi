17eb265f7be6cde4fb1c577e6de23a2633dd0da6
sunswadesh <swadeshp@gmail.com>
2022-02-07 13:25:56 -0400

    Testing ode solvers
    Pre windmi tests.

--- windmiscriptVer0.m
+++ windmiscriptVer0.m
@@ -3,3 +3,11 @@
 % 1.Solve connected ODEs of the windmi-RC model.
 % 2. Tunable/time-varying parameters
 %% %%%%%%%%%%%%%%
+clc;close all;
+tspan = [0 20];
+y0 = 1;
+[t1,y1] = ode23s(@(t,y) -10*t, tspan, y0);
+plot(t1,y1,'r--');
+hold on; grid on; axis tight
+[t2,y2] = ode15s(@(t,y) -10*t, tspan, y0);
+plot(t2,y2,'k--')
